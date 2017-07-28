2b2754fe9bd8307b181564035e7126a67d6151b1
senammu <senthaagilan@gmail.com>
2017-07-28 11:26:36 +0530

    Update pos.c

--- pos.c
+++ pos.c
@@ -1,1 +1,12 @@
-hjjgjghl,
+#include<stdio.h>
+int main()
+{
+ int n;
+ printf("entyer the number");
+ scanf("%d",&n);
+ if(n>0)
+  printf("it s positive ");
+ else
+  printf("it is negative");
+ return 0;
+}
